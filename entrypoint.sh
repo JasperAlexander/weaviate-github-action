828b9a275eb9a142863f70b866cc4525bd86a45c
Jasper Alexander <84851956+JasperAlexander@users.noreply.github.com>
2023-05-27 19:13:53 +0200

    Fixed issue with env vars in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ echo "  - version [$WEAVIATE_VERSION]"
 echo "  - port [$WEAVIATE_PORT]"
 echo ""
 
-docker run --name weaviate --publish $WEAVIATE_PORT:$WEAVIATE_PORT --env QUERY_DEFAULTS_LIMIT=25 AUTHENTICATION_ANONYMOUS_ACCESS_ENABLED=true PERSISTENCE_DATA_PATH=/var/lib/weaviate DEFAULT_VECTORIZER_MODULE=none CLUSTER_HOSTNAME=node1 --detach semitechnologies/weaviate:$WEAVIATE_VERSION
+docker run --name weaviate --publish $WEAVIATE_PORT:$WEAVIATE_PORT --env QUERY_DEFAULTS_LIMIT=25 --env AUTHENTICATION_ANONYMOUS_ACCESS_ENABLED=true --env PERSISTENCE_DATA_PATH='/var/lib/weaviate' --env DEFAULT_VECTORIZER_MODULE='none' --env CLUSTER_HOSTNAME='node1' --detach semitechnologies/weaviate:$WEAVIATE_VERSION
 
 if [ $? -ne 0 ]; then
     echo "Error starting Weaviate in Docker container"
